2e829df8ea24b209768a52b61d4ed9b77a00a0c2
dennis githinji <wakahiad@gmail.com>
2024-01-20 01:36:12 +0300

    loans service openapi docs update

--- src/main/java/org/eazybank/loansservice/LoansServiceApplication.java
+++ src/main/java/org/eazybank/loansservice/LoansServiceApplication.java
@@ -1,9 +1,36 @@
 package org.eazybank.loansservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "EazyBank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Dennis Githinji",
+                        email = "wakahiad@gmail.com",
+                        url = "https://dennisgithinji.tech"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://dennisgithinji.tech"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Loans microservice REST API Documentation",
+                url = "https://dennisgithinji.tech"
+        )
+)
 public class LoansServiceApplication {
 
     public static void main(String[] args) {
